91f0ad6ab623e031861f2a61f26aa5289aa56405
Ramachandra51 <Ramachandra51@users.noreply.github.com>
2019-10-23 22:48:09 -0500

    Initial checkin

---
+++ StringKataSol/StringKataSol/AddNumbersTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringKataInterview.test
+{
+    [TestFixture]
+    public class AddNumbersTest
+    {
+        [Test]
+        public void ReturnSumNumberEmptyReturns0Task1()
+        {
+            var rtn = StringKata.AddNumbers("");
+            Assert.AreEqual(0, rtn);
+        }
+
+        [Test]
+        public void ReturnSumOfNumbersCommaSeperatedTwoVariablesTask1()
+        {
+            var rtn = StringKata.AddNumbers("1,2");
+            Assert.AreEqual(3, rtn);
+        }
+
+        [Test]
+        public void ReturnSumOfNumbersCommaSeperatedManyVariablesTask2()
+        {
+            var rtn = StringKata.AddNumbers("1,2,3,4,5,6");
+            Assert.AreEqual(21, rtn);
+        }
+
+        [Test]
+        public void ReturnSumOfNumbersCommaAndLineBreakSeperatedManyVariablesTask3()
+        {
+            var rtn = StringKata.AddNumbers("1,2,3,4\n5,6");
+            Assert.AreEqual(21, rtn);
+        }
+
+        [Test]
+        public void ReturnSumNumberSeperatorIsPassedInTask4()
+        {
+            var rtn = StringKata.AddNumbers("//;\n1,2,3,4;5,6");
+            Assert.AreEqual(21, rtn);
+        }
+
+        [Test]
+        public void ReturnSumNumberSeperatorIsPassedInTask5()
+        {
+            var rtn = StringKata.AddNumbers("//[***]\n12***2***3");
+            Assert.AreEqual(6, rtn);
+        }
+
+        //[Test]
+        //public void ThrowexceptionOnNegative()
+        //{
+        //    try
+        //    {
+        //        var rtn = StringKata.AddNumbers("-1,1,-6");
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Assert.Equals("negatives not allowed -1 -6", ex.Message);
+        //    }
+        //}
+    }
+}
---
+++ StringKataSol/StringKataSol/StringKata.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringKataInterview
+{
+    class StringKata
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers("");
+        }
+
+        public static int AddNumbers(string s)
+        {
+            int returnValue = 0;
+            if (!string.IsNullOrEmpty(s))
+            {
+                var splitString = s.Split(',','\n',';');
+
+                if (splitString.Length>1)
+                {
+                    int temp = 0;
+                    for (int i = 0; i < splitString.Length; i++)
+                    {
+                        int number;
+                        var isValid = int.TryParse(splitString[i], out number);
+                        if (isValid)
+                        {
+                            temp = temp + Convert.ToInt32(splitString[i]);
+                        }
+                        
+                    }
+                    returnValue = temp;
+                }
+                else
+                {
+                    returnValue =Convert.ToInt32(s);
+                }
+            }
+            return returnValue;
+        }
+    }
+}
